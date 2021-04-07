b8a2adfcff334dd48d8b382020d99d0f45de1313
rafiyq <rafiyqw@tutanota.com>
2021-04-07 21:43:24 +0700

    Change wallpaper and fix status bar

--- sway/config.d/custom_statusbar.sh
+++ sway/config.d/custom_statusbar.sh
@@ -1,45 +1,50 @@
 #!/bin/sh
 
 net() {
-	if=wlp3s0
-  ssid=$(iwconfig $if | sed -e 's/"/ /g' |\
-  awk '/ESSID/{print$5}')
-	if test -z "$ssid"
-	then
-	    ip=$(ip route get 8.8.8.8 2>/dev/null |\
+	  if=wlp3s0
+    ssid=$(iwconfig $if | sed -e 's/"/ /g' |\
+    awk '/ESSID/{print$5}')
+	  if test -z "$ssid"
+	  then
+	      ip=$(ip route get 8.8.8.8 2>/dev/null |\
         grep -Eo 'src [0-9.]+'|grep -Eo '[0-9.]+')
-	    printf "ip:%s" "${ip}"
-	else
-      signal=$(iwconfig $if | sed -e 's/\// /g;s/=/ /g' |\
-      awk '/Quality/{printf("%.0f\n", $3*100/$4)}')
-      printf "%s(%d%%)" "${ssid}" "${signal}"
-	fi
+	      printf "ip:%s" "${ip}"
+	  else
+        signal=$(iwconfig $if | sed -e 's/\// /g;s/=/ /g' |\
+        awk '/Quality/{printf("%.0f\n", $3*100/$4)}')
+        printf "%s(%d%%)" "${ssid}" "${signal}"
+	  fi
 }
 
-temp(){
-	test -f /sys/class/thermal/thermal_zone0/temp || return 0
-	head -c 2 /sys/class/thermal/thermal_zone0/temp
+temp() {
+	  test -f /sys/class/thermal/thermal_zone0/temp || return 0
+	  head -c 2 /sys/class/thermal/thermal_zone0/temp
 }
 
-bat(){
-	hash acpi || return 0
-	onl="$(acpi -V | ag on-line)"
-	charge="$(awk '{ sum += $1 } END { print sum }' /sys/class/power_supply/BAT*/capacity)"
-	if test -z "$onl"
-	then
-		printf "%s" "bat:${charge}%"
-	else
-		printf "%s" "char:${charge}%"
-	fi
+bat() {
+	  hash acpi || return 0
+	  onl="$(acpi -V | ag on-line)"
+	  charge="$(awk '{ sum += $1 } END { print sum }' /sys/class/power_supply/BAT*/capacity)"
+	  if test -z "$onl"
+	  then
+		    printf "%s" "bat:${charge}%"
+	  else
+		    printf "%s" "char:${charge}%"
+	  fi
 }
 
-mem=$(awk '/Mem/{print$2}' /proc/meminfo |\
-awk 'BEGIN {RS = "";FS = "\n"} ; {printf("%.0f\n", 100-$3*100/$1)}')
-formated_date=$(date "+%a-%d-%b %R")
+mem() {
+    awk '/Mem/{print$2}' /proc/meminfo |\
+    awk 'BEGIN {RS = "";FS = "\n"} ; {printf("%.0f\n", 100-$3*100/$1)}'
+}
+
+
+formated_date() {
+    date "+%a-%d-%b %R"
+}
 
 print_status() {
-    printf "ram:%d%%  temp:%dC  %s  %s  %s\n" "${mem}" "$(temp)" "$(net)" "$(bat)" "${formated_date}"
+    printf "ram:%d%%  temp:%dC  %s  %s  %s\n" "$(mem)" "$(temp)" "$(net)" "$(bat)" "$(formated_date)"
 }
 
-#printf "ram:%d%% temp:%dC %s %s %s\n" "${mem}" "$(temp)" "$(net)" "$(bat)" "${formated_date}"
 while true; do print_status; sleep 5; done
