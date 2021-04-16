b7b50b540933a14db7c6d6a18af1731e2457ee0b
rafiyq <rafiyq@pop-os.localdomain>
2021-04-17 05:22:12 +0700

    Add alacritty configuration file

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-ln -sfv $PWD/sway ~/.config/
-ln -sfv $PWD/waybar ~/.config/
+ln -sfv $PWD/alacritty ~/.config/
 ln -sfv $PWD/kitty ~/.config/
-ln -sfv $PWD/wofi ~/.config/
 ln -sfv $PWD/mpv ~/.config/
 ln -sfv $PWD/mako ~/.config/
-ln -sfv $PWD/user-dirs.dirs ~/.config/
+ln -sfv $PWD/sway ~/.config/
+ln -sfv $PWD/waybar ~/.config/
+ln -sfv $PWD/wofi ~/.config/
+#ln -sfv $PWD/user-dirs.dirs ~/.config/
