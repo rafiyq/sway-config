5df75ccdacc78d64e8b6e4198c516d14202b12da
rafiyq <rafiyq@protonmail.com>
2021-04-19 20:50:18 +0700

    add gtk-3 and i3status

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ ln -sfv $PWD/alacritty ~/.config/
 ln -sfv $PWD/mpv ~/.config/
 ln -sfv $PWD/mako ~/.config/
 ln -sfv $PWD/sway ~/.config/
+#ln -sfv $PWD/i3status-rust ~/.config/
 # ln -sfv $PWD/waybar ~/.config/
 ln -sfv $PWD/wofi ~/.config/
+ln -sfv $PWD/gtk-3.0 ~/.config/
 ln -sfv $PWD/user-dirs.dirs ~/.config/
