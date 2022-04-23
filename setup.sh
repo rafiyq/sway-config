e8b44e9f0aa94b24b193d0050ea776cb1c03422a
rafiyq <rafiyq@protonmail.com>
2022-04-23 13:54:09 +0700

    create new sway config

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-ln -sfv $PWD/alacritty ~/.config/
+#ln -sfv $PWD/alacritty ~/.config/
+ln -sfv $PWD/foot ~/.config/
+ln -sfv $PWD/i3status-rust ~/.config/
 #ln -sfv $PWD/mako ~/.config/
 ln -sfv $PWD/sway ~/.config/
-ln -sfv $PWD/i3status-rust ~/.config/
 #ln -sfv $PWD/waybar ~/.config/
-ln -sfv $PWD/wofi ~/.config/
+#ln -sfv $PWD/wofi ~/.config/
 
